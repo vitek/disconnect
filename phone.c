35e31236537ebfe3f22427bc37fc88151a64ac17
Vitja Makarov <vitja.makarov@gmail.com>
2011-03-18 21:19:22 +0300

    Change license to MIT

--- phone.c
+++ phone.c
@@ -1,20 +1,3 @@
-/* Firmware for Disconnect project
- *
- * Copyright (C) 2005 Vitja Makarov <vitja.makarov@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
 #include <avr/io.h>
 #include <avr/signal.h>
 #include <avr/interrupt.h>
