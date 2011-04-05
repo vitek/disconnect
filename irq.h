c8ac0d3eb33d38f49389ac5e215d2794c9dbd49d
Vitja Makarov <vitja.makarov@gmail.com>
2011-04-05 10:01:02 +0400

    Oops add avr/interrupt.h

--- irq.h
+++ irq.h
@@ -1,6 +1,7 @@
 #ifndef IRQ_H
 #define IRQ_H
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define local_irq_save(x)       do {(x) = SREG; cli(); } while (0)
 #define local_irq_restore(x)    do {SREG = (x); } while (0)
