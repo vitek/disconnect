23c65b0f519394a9cd872a12f138a24585c5efc1
Vitja Makarov <vitja.makarov@gmail.com>
2011-04-05 10:01:27 +0400

    Add read/write and reset

--- at45.c
+++ at45.c
@@ -1,9 +1,16 @@
 #include <util/delay.h>
 
 #include "at45.h"
+#include "irq.h"
 
 #define OP_READ_STATUS          0xd7
 #define OP_READ_CONTINUOUS      0xe8
+
+/* write to buffer, then write-erase to flash */
+#define OP_PROGRAM_VIA_BUF1     0x82
+#define OP_PROGRAM_VIA_BUF2     0x85
+
+#define PAGE_OFFSET 11
 
 static inline
 void at45_select()
@@ -31,10 +38,69 @@ unsigned char at45_status_read()
     return b;
 }
 
+int at45_write_page(unsigned int page, const char *data)
+{
+    unsigned int addr;
+    unsigned char status;
+    int i;
+
+    if (page >= AT45_NR_PAGES)
+        return -1;
+
+    addr = page << (PAGE_OFFSET - 8);
+
+    at45_select();
+    at45_spi_write(OP_PROGRAM_VIA_BUF1);
+    at45_spi_write(addr >> 8);
+    at45_spi_write(addr & 0xff);
+
+    for (i = 0; i < AT45_PAGE_SIZE; i++)
+        at45_spi_write(data[i]);
+
+    at45_deselect();
+
+    do {
+        status = at45_status_read();
+    } while (0 == (status & 0x80));
+
+    return 0;
+}
+
+int at45_read_start(unsigned int page)
+{
+    unsigned int addr;
+
+    if (page >= AT45_NR_PAGES)
+        return -1;
+
+    addr = page << (PAGE_OFFSET - 8);
+
+    at45_select();
+    at45_spi_write(OP_READ_CONTINUOUS);
+    at45_spi_write(addr >> 8);
+    at45_spi_write(addr & 0xff);
+
+    /* plus 4 "don't care" bytes */
+    at45_spi_write(0);
+    at45_spi_write(0);
+    at45_spi_write(0);
+    at45_spi_write(0);
+
+    return 0;
+}
+
+void at45_read_end()
+{
+    at45_deselect();
+}
+
 int at45_init()
 {
     unsigned char status;
+    unsigned char flags;
 
+
+    local_irq_save(flags);
     // MOSI and CLK are outputs
     DDRB |= (1 << PB2) | (1 << PB1) | (1 << PB0);
     DDRB &= ~(1 << PB3);
@@ -43,6 +109,7 @@ int at45_init()
     SPCR = (1 << SPE) | (1 << DORD) | (1 << MSTR) | (1 << CPOL) | (1 << CPOL);
 
     at45_deselect();
+    local_irq_restore(flags);
 
     status = at45_status_read();
 
@@ -55,4 +122,15 @@ int at45_init()
     }
 
     return 0;
+}
+
+void at45_reset()
+{
+    unsigned char flags;
+
+    local_irq_save(flags);
+    SPCR = 0;
+    DDRB &= ~((1 << PB2) | (1 << PB1) | (1 << PB0));
+    at45_deselect();
+    local_irq_restore(flags);
 }
--- at45.h
+++ at45.h
@@ -3,6 +3,9 @@
 #define DISCONNECT_AT45_H
 #include <avr/io.h>
 
+#define AT45_PAGE_SIZE 1056
+#define AT45_NR_PAGES  8192
+
 static inline
 unsigned char at45_spi_read()
 {
@@ -20,5 +23,23 @@ void at45_spi_write(unsigned char b)
 }
 
 int at45_init();
+void at45_reset();
+
+/**
+ * Write single page.
+ */
+int at45_write_page(unsigned int page, const char *data);
+
+/**
+ * Issue continuous read command.
+ * Bytes should be read manually.
+ */
+int at45_read_start(unsigned int page);
+
+/**
+ * Finish continuous read operation, deselect device.
+ */
+void at45_read_end();
+
 
 #endif /* DISCONNECT_AT45_H */
