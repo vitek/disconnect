7b6072a5bdaaed33d1ff526b12d5e6449403ee03
Vitja Makarov <vitja.makarov@gmail.com>
2011-04-08 09:58:39 +0400

    Async uart receive

--- uart.c
+++ uart.c
@@ -1,10 +1,12 @@
+#include <avr/interrupt.h>
+
 #include "uart.h"
 
 void uart0_init(unsigned int baud)
 {
     unsigned char cfg;
 
-    cfg = (unsigned char) (1 << RXEN) | (1 << TXEN);
+    cfg = (unsigned char) (1 << RXEN) | (1 << TXEN) | (1 << RXCIE0);
 
     /* Set baud rate */
     UBRR0H = (unsigned char) (baud >> 8);
@@ -23,4 +25,16 @@ void uart0_reset()
     /* disable uart */
     UCSR0B = 0;
     UCSR0A = 0;
+}
+
+volatile char _uart0_buf[UART_BUF_SIZE];
+volatile unsigned char _uart0_buf_pos = 0;
+volatile unsigned char _uart0_buf_len = 0;
+
+SIGNAL(SIG_UART0_RECV)
+{
+    _uart0_buf[_uart0_buf_pos] = UDR0;
+    _uart0_buf_pos = (_uart0_buf_pos + 1) & (UART_BUF_SIZE - 1);
+    if (_uart0_buf_len < UART_BUF_SIZE)
+        _uart0_buf_len++;
 }
--- uart.h
+++ uart.h
@@ -2,8 +2,11 @@
 #define DISCONNECT_UART_H
 
 #define UART_BAUD(b) (((F_CPU) / (8 * (b))) - 1)
+#define UART_BUF_SIZE 64
 
 #include <avr/io.h>
+
+#include "irq.h"
 
 void uart0_init(unsigned int baud);
 void uart0_reset();
@@ -16,8 +19,30 @@ void uart0_putc(unsigned char c)
     UDR0 = c;
 }
 
+extern volatile char _uart0_buf[UART_BUF_SIZE];
+extern volatile unsigned char _uart0_buf_pos;
+extern volatile unsigned char _uart0_buf_len;
+
 static inline
 unsigned char uart0_getc(unsigned char *c)
+{
+    unsigned char flags;
+
+    local_irq_save(flags);
+
+    if (0 == _uart0_buf_len) {
+        local_irq_restore(flags);
+        return 0;
+    }
+
+    *c = _uart0_buf[(_uart0_buf_pos - _uart0_buf_len) & (UART_BUF_SIZE - 1)];
+    _uart0_buf_len--;
+    local_irq_restore(flags);
+    return 1;
+}
+
+static inline
+unsigned char uart0_getc_noi(unsigned char *c)
 {
     if ( !(UCSR0A & (1<<RXC)))
         return 0;
@@ -33,13 +58,20 @@ void uart0_puts(const char *msg)
 }
 
 static inline
-void uart0_print_hex(char c)
+void uart0_print_hex(unsigned char c)
 {
-    char c0 = c >> 4;
-    char c1 = c & 0xf;
+    unsigned char c0 = c >> 4;
+    unsigned char c1 = c & 0xf;
 
     uart0_putc(c0 > 9 ? c0 + 'a' - 10: c0 + '0');
     uart0_putc(c1 > 9 ? c1 + 'a' - 10: c1 + '0');
+}
+
+static inline
+void uart0_print_hex16(unsigned short c)
+{
+    uart0_print_hex(c >> 8);
+    uart0_print_hex(c & 0xff);
 }
 
 #endif /* DISCONNECT_UART_H */
